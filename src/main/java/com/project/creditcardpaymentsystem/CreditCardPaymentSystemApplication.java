5446de21331fec4806449e44f2e1c236712d797a
anshraj-singh <anshrajsingh6565555@gmail.com>
2024-12-07 17:36:48 +0530

    first commit to upload credit card payment system all files

---
+++ src/main/java/com/project/creditcardpaymentsystem/CreditCardPaymentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.project.creditcardpaymentsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditCardPaymentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditCardPaymentSystemApplication.class, args);
+	}
+
+}
