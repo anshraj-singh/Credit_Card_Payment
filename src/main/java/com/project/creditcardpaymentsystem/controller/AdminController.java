5abb677520d6bcf6337f8c6b8c0865a4a63f7215
anshraj-singh <anshrajsingh6565555@gmail.com>
2025-01-01 15:53:28 +0530

    Moved monthly report endpoint to AdminController

--- src/main/java/com/project/creditcardpaymentsystem/controller/AdminController.java
+++ src/main/java/com/project/creditcardpaymentsystem/controller/AdminController.java
@@ -1,13 +1,16 @@
 package com.project.creditcardpaymentsystem.controller;
 
 import com.project.creditcardpaymentsystem.entity.Customer;
+import com.project.creditcardpaymentsystem.entity.User;
 import com.project.creditcardpaymentsystem.service.CustomerService;
+import com.project.creditcardpaymentsystem.service.ReportService;
+import com.project.creditcardpaymentsystem.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,6 +21,12 @@ public class AdminController {
     @Autowired
     private CustomerService customerService;
 
+    @Autowired
+    private UserService userService; // Inject UserService
+
+    @Autowired
+    private ReportService reportService;
+
     // Get all customers - Only accessible by admin
     @GetMapping("/all-customers")
     public ResponseEntity<?> getAllCustomers() {
@@ -26,5 +35,26 @@ public class AdminController {
             return new ResponseEntity<>(allCustomers, HttpStatus.OK);
         }
         return new ResponseEntity<>("No customers found.", HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping("/reports/monthly")
+    public ResponseEntity<?> generateMonthlyReport(@RequestParam String customerId, @RequestParam int month, @RequestParam int year) {
+        try {
+            // Get the currently authenticated user's username
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            String userName = authentication.getName();
+
+            // Find the user by username
+            User user = userService.findByUsername(userName);
+            if (user == null) {
+                return new ResponseEntity<>("User  not found.", HttpStatus.NOT_FOUND);
+            }
+
+            // Call the report service to generate the report
+            reportService.generateMonthlyReport(user.getId(), customerId, month, year);
+            return new ResponseEntity<>("Monthly report generated and sent to email.", HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Error generating report: " + e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
     }
 }
